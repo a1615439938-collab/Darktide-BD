84061f3b7bae4a8fe6c77d0e1442015cd9a6dc97
aabbcc <a1615439938@gmail.com>
2026-09-20 00:17:48 +0800

    Cache lightweight talent core instead of heavy icon bundle

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="darktide-bd-tree-v4";
-const ASSETS=["./","./index.html","./styles.css","./tree-data.js","./app.js","./manifest.webmanifest"];
+const CACHE="darktide-bd-tree-v5";
+const ASSETS=["./","./index.html","./styles.css","./tree-core.js","./app.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))]))});
 self.addEventListener("fetch",e=>{
