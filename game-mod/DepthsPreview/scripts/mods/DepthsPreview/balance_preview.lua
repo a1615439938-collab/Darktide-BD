ce201ada7c0295156719cdef0ca65326544affa9
aabbcc <a1615439938@gmail.com>
2026-09-27 01:04:34 +0800

    Apply Veteran preview stamina regeneration delay

--- game-mod/DepthsPreview/scripts/mods/DepthsPreview/balance_preview.lua
+++ game-mod/DepthsPreview/scripts/mods/DepthsPreview/balance_preview.lua
@@ -1,7 +1,7 @@
 local mod = get_mod("DepthsPreview")
 
 local Archetypes = require("scripts/settings/archetype/archetypes")
-local ToughnessTemplates = require("scripts/settings/toughness/archetype_toughness_templates")
+local ToughnessTemplates = require("scripts/settings/toughness/archetype_toughness_templates")\nlocal StaminaTemplates = require("scripts/settings/stamina/archetype_stamina_templates")
 
 local Balance = {}
 local originals = nil
@@ -14,7 +14,7 @@ local function capture()
     veteran_toughness = ToughnessTemplates.veteran.max,
     zealot_toughness = ToughnessTemplates.zealot.max,
     ogryn_toughness = ToughnessTemplates.ogryn.max,
-    psyker_crit = Archetypes.psyker.base_critical_strike_chance,
+    psyker_crit = Archetypes.psyker.base_critical_strike_chance,\n    veteran_stamina_regen_delay = StaminaTemplates.veteran.regeneration_delay,
   }
 end
 
@@ -25,7 +25,7 @@ function Balance.apply()
   ToughnessTemplates.veteran.max = 120
   ToughnessTemplates.zealot.max = 125
   ToughnessTemplates.ogryn.max = 125
-  Archetypes.psyker.base_critical_strike_chance = 0.10
+  Archetypes.psyker.base_critical_strike_chance = 0.10\n  StaminaTemplates.veteran.regeneration_delay = 0.5
 end
 
 function Balance.restore()
@@ -35,7 +35,7 @@ function Balance.restore()
   ToughnessTemplates.veteran.max = originals.veteran_toughness
   ToughnessTemplates.zealot.max = originals.zealot_toughness
   ToughnessTemplates.ogryn.max = originals.ogryn_toughness
-  Archetypes.psyker.base_critical_strike_chance = originals.psyker_crit
+  Archetypes.psyker.base_critical_strike_chance = originals.psyker_crit\n  StaminaTemplates.veteran.regeneration_delay = originals.veteran_stamina_regen_delay
 end
 
 return Balance
